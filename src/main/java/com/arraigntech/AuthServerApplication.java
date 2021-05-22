300f64066c04464e595617ec26fdf28c74db6a9f
flashbhaskar <bhaskar@arraigntech.com>
2021-05-22 17:44:27 +0530

    changes

--- src/main/java/com/arraigntech/AuthServerApplication.java
+++ src/main/java/com/arraigntech/AuthServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
