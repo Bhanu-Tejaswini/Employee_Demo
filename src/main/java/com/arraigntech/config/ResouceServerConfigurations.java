d25e1de796139dcb4a083a9cd4e5f13dc2f0b1d8
Manojkumar Mathiarasan <manojitsoft@hotmail.com>
2021-05-31 20:40:04 +0530

    Changes

--- src/main/java/com/arraigntech/config/ResouceServerConfigurations.java
+++ src/main/java/com/arraigntech/config/ResouceServerConfigurations.java
@@ -3,7 +3,6 @@ package com.arraigntech.config;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -24,24 +22,6 @@ public class ResouceServerConfigurations extends ResourceServerConfigurerAdapter
 	
 	public final static String CSRF_HEADER_NAME =  "Access-Control-Allow-Origin";
 	
-	private static final String[] AUTH_WHITELIST = {
-            // -- Swagger UI v2
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            // -- Swagger UI v3 (OpenAPI)
-            "/v3/api-docs/**",
-            "/swagger-ui/**"
-            // other public endpoints of your API may be appended to this array
-    };
-
-	@Autowired
-	private ResourceServerTokenServices tokenServices;
-
 	@Value("${security.jwt.resource-ids}")
 	private String resourceIds;
 
@@ -69,9 +49,9 @@ public class ResouceServerConfigurations extends ResourceServerConfigurerAdapter
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		final CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedOriginPatterns(Arrays.asList("*"));
 		config.setAllowedHeaders(Arrays.asList("*"));
-		config.addExposedHeader(CSRF_HEADER_NAME);
+		config.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
 		config.setAllowedMethods(Arrays.asList("OPTIONS", "HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
